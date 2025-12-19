132e838b39329c298966ef80eae33236196a3130
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2025-12-19 14:56:39 +0530

    [Loveesh Gulati] Refactor. Updated Datatype.cs

--- core-csharp-practice/gcr-codebase/programming-elements/Datatype.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/Datatype.cs
@@ -7,5 +7,27 @@ class Datatype{
 	short ci = 100;
 	float cl = 3.04f;
 	double cp = 3.09;
+	char cou = 'could';
+	bool isPass = true;
+    string wool = "sheep";
+	int[] numbers = {1,2,3,4};
+		public void StartEngine(){
+			Console.WriteLine("Engine Started");
+		}
+	Car myCar = new Car();
+	myCar.model = Toyota;
+	myCar.year = 2020;
+	myCar.startEngine();
 	}
+	public enum Day{
+		Monday, Tuesday, Wednesday, Thursday
+	}
+	Day today = Day.Monday;
+	//Implicit CONVERSION 
+	int a = 8;
+	double b = a;
+	Console.WriteLine(b);
+	//Explicit CONVERSION
+	float c = 10.4f;
+	long b = (long)c;
 }
