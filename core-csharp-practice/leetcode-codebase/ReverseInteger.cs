d05b2f22d1454dc7cf51263a5024a97e11f95f0f
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2025-12-22 19:51:54 +0530

    [Loveesh Gulati] Refactor. Fixed Leetcode Problem

--- core-csharp-practice/leetcode-codebase/ReverseInteger.cs
+++ core-csharp-practice/leetcode-codebase/ReverseInteger.cs
@@ -1,18 +1,19 @@
-   
-   public class ReverseInteger{
-   public int reverse(int n){
-     long rev = 0;
+public class ReverseInteger
+{
+    public int Reverse(int n)
+    {
+        long rev = 0;
 
-        while (n != 0){
-            rev = rev * 10 + (n % 10);
-            n = n / 10;
+        while (n != 0)
+        {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
         }
 
-        if (rev > int.MaxValue || rev < int.MinValue){   
-		return 0;
-        }
+        if (rev > int.MaxValue || rev < int.MinValue)
+            return 0;
 
         return (int)rev;
     }
-   
-   }
+}
