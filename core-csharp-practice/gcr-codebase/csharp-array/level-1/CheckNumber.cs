c8db3e98d43c7b018f272817b7e6bef87d8241f6
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2025-12-24 11:37:01 +0530

    [Loveesh Gulati] Add. Added Level-1 Solutions For Array

---
+++ core-csharp-practice/gcr-codebase/csharp-array/level-1/CheckNumber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CGProject.Bridge
+{
+    public class CheckNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter Number " + (i + 1) + " :");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for(int i=0;i< numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    Console.WriteLine("Negative Number :");
+                }
+                else if (numbers[i] == 0)
+                {
+                    Console.WriteLine("Zero");
+                }
+                else
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        Console.WriteLine("Even Number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Odd Number");
+                    }
+                }
+                
+            }
+            if (numbers[0] == numbers[numbers.Length - 1])
+            {
+                Console.WriteLine("The first element and last number are equal");
+            }
+            else if (numbers[0] < numbers[numbers.Length - 1])
+            {
+                Console.WriteLine("The first element is less than last element");
+            }
+            else
+            {
+                Console.WriteLine("The first element is greater than last number");
+            }
+
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-array/level-1/TwoDimensionalArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CGProject.Bridge
+{
+    public class TwoDimensionalArray
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows :");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number of columns :");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array_two_d = new int[rows,columns];
+            Console.WriteLine("Enter the elements of 2D array :");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++) {
+                    array_two_d[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            int[] array_one_d = new int[rows * columns];
+            int index = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    array_one_d[index]= array_two_d[i,j];
+                    index++;
+                }
+            }
+            for (int i = 0; i < array_one_d.Length; i++) { 
+                Console.WriteLine(array_one_d [i]+ " ");
+            }
+
+            
+        }
+    }
+}
