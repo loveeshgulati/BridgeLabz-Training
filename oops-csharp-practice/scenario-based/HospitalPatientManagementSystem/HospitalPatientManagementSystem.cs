c6cc4e50209e42f9fad82dcb1ef849378f1ab373
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-01-05 15:13:40 +0530

    [Loveesh Gulati] Add. Added scenario-based solution

---
+++ oops-csharp-practice/scenario-based/HospitalPatientManagementSystem/HospitalPatientManagementSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.OOPS.HospitalPatientManagementSystem
+{
+    public class HospitalPatientManagementSystem
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor = new Doctor("Dr. Rama", "Cardiologist");
+            doctor.ShowDoctorDetails();
+
+            Patient patient1 = new InPatient(221500094,"Dev",3,50000);
+            Patient patient2 = new OutPatient(2000, 221500100, "Priyanshu");
+
+            patient1.DisplayInfo();
+            patient2.DisplayInfo();
+
+            Bill.PrintBill((IPayable)patient1);
+            Bill.PrintBill((IPayable)patient2);
+
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/HospitalPatientManagementSystem/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.OOPS.HospitalPatientManagementSystem
+{
+    
+    public abstract class Patient
+    {
+        public int PatientID {  get; private set; }
+        public string PatientName {  get; private set; }
+        protected Patient(int  PatientID, string PatientName)
+        {
+            this.PatientID = PatientID;
+            this.PatientName = PatientName;
+        }
+        public abstract void DisplayInfo();
+
+    }
+}
