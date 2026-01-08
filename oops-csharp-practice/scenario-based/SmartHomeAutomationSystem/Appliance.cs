26372438965abb16acb508a1a1f857de63cc640f
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-01-08 14:36:04 +0530

    [Loveesh Gulati] Add. Added Smart Home Automation System

---
+++ oops-csharp-practice/scenario-based/SmartHomeAutomationSystem/Appliance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.OOPS.SmartHomeAutomationSystem
+{
+    public abstract class Appliance:IControllable
+    {
+        public string Name { set;  get; }
+        protected Appliance(string Name)
+        {
+            this.Name = Name;
+        }
+        public abstract void TurnOn();
+        public abstract void TurnOff();
+
+    }
+}
---
+++ oops-csharp-practice/scenario-based/SmartHomeAutomationSystem/Light.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.OOPS.SmartHomeAutomationSystem
+{
+    public class Light:Appliance
+    {
+        public Light() : base("Light"){ }
+        public override void TurnOn()
+        {
+            Console.WriteLine("Light turned On");
+        }
+        public override void TurnOff()
+        {
+            Console.WriteLine("Light Turned Off");
+        }
+    }
+}
