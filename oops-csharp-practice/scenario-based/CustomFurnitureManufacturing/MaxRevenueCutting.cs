fb6949035f1fb2f4c4482a7e8b5403594895b753
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-01-12 16:27:36 +0530

    [Loveesh Gulati] Add. Added Scenario Based Solution

---
+++ oops-csharp-practice/scenario-based/CustomFurnitureManufacturing/MaxRevenueCutting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Opps_senario.Furniture
+{
+    internal class MaxRevenueCutting:IWoodCuttingStrategy
+    {
+        public int GetMaxRevenue(int length, int[] prices, int allowedWaste) {
+            int[] dp = new int[length + 1];
+            for (int i = 1; i <= length; i++)
+            {
+                int max = 0;
+                for (int cut = 1; cut <= i; cut++)
+                {
+                    max = Math.Max(max, prices[cut] + dp[i - cut]);
+                }
+                dp[i] = max;
+            }
+            return dp[length];
+        }
+    }
+}
