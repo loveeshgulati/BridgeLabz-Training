beff304c8db97a8e65e6a3b3de43868a1b32b407
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-01-12 15:19:08 +0530

    [Loveesh Gulati] Add. Added Scenario Based Solution

---
+++ oops-csharp-practice/scenario-based/MetalFactory/MetalFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Opps_senario.MetalFaactory
+{
+    internal class MetalFactory
+    {
+            static void Main()
+            {
+                new RodCuttingController().Start();
+            }
+        }
+
+    }
