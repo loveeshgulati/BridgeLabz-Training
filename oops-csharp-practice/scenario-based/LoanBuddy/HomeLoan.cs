876dcc87c6e4c1e02729185e31b00bc5a9aeb71b
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-01-13 12:04:23 +0530

    [Loveesh Gulati] Add. Added Loan Buddy Scenario based problem

---
+++ oops-csharp-practice/scenario-based/LoanBuddy/HomeLoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.OOPS.LoanBuddy
+{
+    class HomeLoan : LoanApplication
+    {
+        public HomeLoan(Applicant applicant, int months) : base(applicant, months, 8.5) { }
+        public override double CalculateEMI()
+        {
+            return base.CalculateEMI() * 0.98;
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/LoanBuddy/LoanBuddyMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.OOPS.LoanBuddy
+{
+    internal class LoanBuddyMain
+    {
+        static void Main(String[] args)
+        {
+            new Menu().Start();
+        }
+    }
+
+}
