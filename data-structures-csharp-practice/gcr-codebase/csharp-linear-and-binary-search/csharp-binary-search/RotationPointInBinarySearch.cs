64cfc525b9c6024f265c7eab0a29731c5902b929
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-01-13 19:19:53 +0530

    [Loveesh Gulati] Add. Added Binary Search Solutions

---
+++ data-structures-csharp-practice/gcr-codebase/csharp-linear-and-binary-search/csharp-binary-search/RotationPointInBinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.DataStructures.binarysearch
+{
+    internal class RotationPointInBinarySearch
+    {
+        static void Main()
+        {
+            int[] arr = { 9, 8, 5, 4, 7, 3, 1 };
+
+            int findIndex = FindRotationPoint(arr);
+
+            Console.WriteLine("Index of smallest element: " + findIndex);
+            Console.WriteLine("Smallest element: " + arr[findIndex]);
+        }
+
+        static int FindRotationPoint(int[] arr)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low < high)
+            {
+
+                int mid = low + (high - low) / 2;
+
+
+                if (arr[mid] > arr[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+
+                    high = mid;
+                }
+            }
+
+            return low;
+        }
+    }
+
+}
