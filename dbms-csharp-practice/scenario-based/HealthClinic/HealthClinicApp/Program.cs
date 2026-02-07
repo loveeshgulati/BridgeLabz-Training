7a998f7ebd078d1fe043000d9cf73f2eb59ecd5c
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-02-07 22:06:58 +0530

    [Loveesh Gulati] Refactor : Updated UC-1 Patient Management.

--- dbms-csharp-practice/scenario-based/HealthClinic/HealthClinicApp/Program.cs
+++ dbms-csharp-practice/scenario-based/HealthClinic/HealthClinicApp/Program.cs
@@ -2,6 +2,7 @@
     {
         static void Main(string[] args)
         {
+            ConnectionDB.InitializeDatabase();
             ClinicMenu menu = new ClinicMenu();
 
             while (true)
